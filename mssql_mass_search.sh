68a52ad2a96d20aa7bce814b2a9e586f9bfcdb11
incrig <kpalcins@gmail.com>
2023-05-11 18:55:48 +0300

    Update mssql_mass_search.sh

--- mssql_mass_search.sh
+++ mssql_mass_search.sh
@@ -1,17 +1,26 @@
+# Script to run a mass select on multiple databases on single MSSQL server
 #!/bin/bash
 
+sqlbin="/usr/bin/sqlcmd" # location of mssql binary
 serverName="localhost" # replace with the name of your SQL Server instance
 username="myusername" # replace with your SQL Server username
 password="mypassword" # replace with your SQL Server password
 databasePattern="mydatabase%" # replace with the pattern for the database names you want to query
-query="SELECT * FROM mytable" # replace with the query you want to run
+query="set nocount on; SELECT 1;" # replace with the query you want to run
+
+export SQLCMDPASSWORD # Needed since -P is obsolete and pass cant be used inline
 
 # Loop through each database on the server whose name matches the pattern
-for database in $(/opt/mssql-tools/bin/sqlcmd -S $serverName -U $username -P $password -h -1 -W -Q "SELECT name FROM sys.databases WHERE name LIKE '$databasePattern'") ; do
-    # Run the query and get the results
-    results=$(/opt/mssql-tools/bin/sqlcmd -S $serverName -U $username -P $password -h -1 -W -d $database -Q "$query")
+databases=$($sqlbin -S $serverName -U $username --headers="-1" -W -Q "set nocount on; SELECT name FROM sys.databases WHERE name LIKE '$databasePattern'")
+echo "Existing databases:"
+for database in ${databases[@]}; do echo $database; done
+echo ""
 
+for database in $databases ; do
+    # Run the query and get the results
+    results=$($sqlbin -S $serverName -U $username --headers="-1" -W -d $database -Q "$query")
     # Print the results
     echo "Results from $database:"
     echo "$results"
+    echo ""
 done
